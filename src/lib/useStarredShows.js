95126ac44062b7f2f33633c4a0dc339e29b40040
Dhananjay Yadev <dhananjay16642@gmail.com>
2023-06-20 15:42:31 +0530

    persisted searchform textbox value in session storage. write all the logic in serach

--- src/lib/useStarredShows.js
+++ src/lib/useStarredShows.js
@@ -2,9 +2,9 @@ import { useReducer, useEffect } from 'react';
 
 const usePersistedReducer = (reducer, initialState, localStorageKey) => {
   const [state, dispatch] = useReducer(reducer, initialState, initial => {
-    const presistedValue = localStorage.getItem(localStorageKey);
+    const persistedValue = localStorage.getItem(localStorageKey);
 
-    return presistedValue ? JSON.parse(presistedValue) : initial;
+    return persistedValue ? JSON.parse(persistedValue) : initial;
   });
 
   useEffect(() => {
